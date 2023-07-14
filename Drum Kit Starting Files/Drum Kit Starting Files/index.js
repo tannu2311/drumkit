3b0ac5b2208c02c11cc75cee104b3f5f049eee56
tannu baghel <83713524+tannu2311@users.noreply.github.com>
2023-07-14 15:38:14 +0530

    animation
    added animation to the buttons on screen when clicked either on keyboard or on the website

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,35 +1,16 @@
 
 for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 {document.querySelectorAll("button")[i].addEventListener("click",function(){
-    switch(this.innerHTML){
-        case("w"):
-        var audio= new Audio("sounds/crash.mp3");
-        break;
-        case("a"):
-        var audio= new Audio("sounds/kick-bass.mp3");
-        break;
-        case("s"):
-        var audio= new Audio("sounds/tom-1.mp3");
-        break;
-        case("j"):
-        var audio= new Audio("sounds/tom-2.mp3");
-        break;
-        case("d"):
-        var audio= new Audio("sounds/tom-3.mp3");
-        break;
-        case("k"):
-        var audio= new Audio("sounds/tom-4.mp3");
-        break;
-        case("l"):
-        var audio= new Audio("sounds/snare.mp3");
-        break;
-
-    }
-    audio.play();
-})
+   makesound(this.innerHTML);
+    animate(this.innerHTML);
+})}
 document.addEventListener("keypress",function(e){
-    console.log(e.key)
-    switch(e.key){
+     makesound(e.key);
+    animate(e.key)
+})
+
+function makesound(event){
+     switch(event){
         case("w"):
         var audio= new Audio("sounds/crash.mp3");
         break;
@@ -55,6 +36,12 @@ document.addEventListener("keypress",function(e){
     }
     audio.play();
 }
-)};
+function animate(key){
+    document.querySelector("."+key).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+key).classList.remove("pressed");
+    },100)
+    
+}
 
 
