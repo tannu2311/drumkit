77b396bc4e48df7a0f472af8ce417516cb957b2d
tannu baghel <83713524+tannu2311@users.noreply.github.com>
2023-07-14 14:46:13 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,60 @@
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{document.querySelectorAll("button")[i].addEventListener("click",function(){
+    switch(this.innerHTML){
+        case("w"):
+        var audio= new Audio("sounds/crash.mp3");
+        break;
+        case("a"):
+        var audio= new Audio("sounds/kick-bass.mp3");
+        break;
+        case("s"):
+        var audio= new Audio("sounds/tom-1.mp3");
+        break;
+        case("j"):
+        var audio= new Audio("sounds/tom-2.mp3");
+        break;
+        case("d"):
+        var audio= new Audio("sounds/tom-3.mp3");
+        break;
+        case("k"):
+        var audio= new Audio("sounds/tom-4.mp3");
+        break;
+        case("l"):
+        var audio= new Audio("sounds/snare.mp3");
+        break;
+
+    }
+    audio.play();
+})
+document.addEventListener("keypress",function(e){
+    console.log(e.key)
+    switch(e.key){
+        case("w"):
+        var audio= new Audio("sounds/crash.mp3");
+        break;
+        case("a"):
+        var audio= new Audio("sounds/kick-bass.mp3");
+        break;
+        case("s"):
+        var audio= new Audio("sounds/tom-1.mp3");
+        break;
+        case("j"):
+        var audio= new Audio("sounds/tom-2.mp3");
+        break;
+        case("d"):
+        var audio= new Audio("sounds/tom-3.mp3");
+        break;
+        case("k"):
+        var audio= new Audio("sounds/tom-4.mp3");
+        break;
+        case("l"):
+        var audio= new Audio("sounds/snare.mp3");
+        break;
+
+    }
+    audio.play();
+}
+)};
+
+
